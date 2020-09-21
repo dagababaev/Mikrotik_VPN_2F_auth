90a903dd269ac19a9f3cea885db49178adae685e
Dmitri Agababaev <66842523+dagababaev@users.noreply.github.com>
2020-09-21 15:33:23 +0300

    Add files via upload

---
+++ class/templates/success.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+      <html lang="ru">
+      <meta http-equiv="Content-Type" content="charset=utf-8" />
+      <body style="font-family: Verdana, Arial, Helvetica, sans-serif; background-color: #282c34; color: #fff; height: 100vh; display: flex;">
+        <div style="margin: auto; max-width: 50%;">
+          <p style="font-size: 24pt; font-weight: bold; margin: 0 0 10px;">
+    VPN-соединение установлено, можете продолжить работу<br />
+          </p>
+          <p style="font-size: 12pt; color: #aaa;">
+    В случае недоступности сервисов обратитесь к вашему системному администратору<br />
+          </p>
+          <p style="font-size: 24pt; font-weight: bold; margin: 100px 0 10px;">
+    VPN connection is established, you can continue to work
+</p>
+          <p style="font-size: 12pt; color: #aaa;">
+            If any services unavalible you must contact with your system administrator<br />
+          </p>
+        </div>
+      </body>
+      </html>
